b9540eb10be4b56ef3f9f596fb90344016897752
mohamed <mohamed@intuitiv-interactive.com>
2020-08-27 17:34:39 +0100

    Updates

---
+++ precache-manifest.8d06fdb966a049eec9c007156484eabe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c789349f477df0bf66c8b9e584e7a5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "057b139c3735465ac5b7",
+    "url": "/static/css/main.f4a16f6d.chunk.css"
+  },
+  {
+    "revision": "656ff4a486f663961f70",
+    "url": "/static/js/2.5ab7ce02.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.5ab7ce02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057b139c3735465ac5b7",
+    "url": "/static/js/main.41d52204.chunk.js"
+  },
+  {
+    "revision": "7a7d93ce103432ddb888",
+    "url": "/static/js/runtime-main.4fdbab1b.js"
+  }
+]);
