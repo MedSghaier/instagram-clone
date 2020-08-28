e08c63ff14ff47421a5658103dd934a6148ccd78
mohamed <mohamed@intuitiv-interactive.com>
2020-08-28 10:13:40 +0100

    Updates

---
+++ precache-manifest.d3468a953148409caa608cbbaa5c93c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a2191fc5f2c549c633ab1221467527",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "b0c54a8dbbe37888302f",
+    "url": "/instagram-clone/static/css/main.f4a16f6d.chunk.css"
+  },
+  {
+    "revision": "61cdb1fa4066f3876117",
+    "url": "/instagram-clone/static/js/2.68fb886d.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/instagram-clone/static/js/2.68fb886d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c54a8dbbe37888302f",
+    "url": "/instagram-clone/static/js/main.febaf69c.chunk.js"
+  },
+  {
+    "revision": "57c1bb362b002093f3dc",
+    "url": "/instagram-clone/static/js/runtime-main.0422fd05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d06fdb966a049eec9c007156484eabe.js"
+  "/instagram-clone/precache-manifest.d3468a953148409caa608cbbaa5c93c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/instagram-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
